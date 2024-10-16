be6f024695af2d2f8ba796bc415e0c93ca3235d7
DmMin417 <abcod37377@gmail.com>
2024-10-16 11:28:15 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,91 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2",
+      "name": "파노라마2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.663372528797538,
+          "pitch": 0.07662255112616201,
+          "rotation": 0,
+          "target": "1-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.81590079636835,
+          "pitch": -0.4525210828033739,
+          "title": "복지센터&nbsp;",
+          "text": "복지센터 정면<div>느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다느낌을 설명한다</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.1315705716610598,
+        "pitch": 0.0017635761593162158,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.194829564888618,
+          "pitch": 0.08735058931184625,
+          "rotation": 0,
+          "target": "0-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22563296177982117,
+          "pitch": 0.03709200619553954,
+          "title": "주 출입구",
+          "text": "이용자들의 출입이 주로 이루어지는 공간"
+        },
+        {
+          "yaw": -0.40111143954413997,
+          "pitch": 0.3309107052965743,
+          "title": "1층 로비",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
